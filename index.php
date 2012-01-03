493724a3e78c14b785919dcae0dcb4dd56d9062b
Joseph McKenney <j.mckenney00@gmail.com>
2012-01-03 10:33:07 -0800

    inital commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title> joe mckenney </title>
+	
+		<link  href="http://cs.mills.edu/~jmckenney/style.css" rel="stylesheet"    type="text/css" /> 
+	</head>
+	<script language="Javascript"> 
+	<!-- 
+	function ShowPicture(id,Source) { 
+	if (Source=="1"){ 
+	if (document.layers) document.layers[''+id+''].visibility = "show" 
+	else if (document.all) document.all[''+id+''].style.visibility = "visible" 
+	else if (document.getElementById) document.getElementById(''+id+'').style.visibility = "visible" 
+	} 
+	else 
+	if (Source=="0"){ 
+	if (document.layers) document.layers[''+id+''].visibility = "hide" 
+	else if (document.all) document.all[''+id+''].style.visibility = "hidden" 
+	else if (document.getElementById) document.getElementById(''+id+'').style.visibility = "hidden" 
+	} 
+	} 
+	//--> 
+	</script>
+	<body>
+	
+		<!--Start of bodybox -->
+		<div id="bodyBox">
+			
+				
+				
+				<!--Start of Navigation Bar -->
+				<div class="nav_bar">
+				
+					<table height="25" align="center"  class="menu"> 
+				  		<tr> 
+				    		<td ><a onMouseOver="ShowPicture('mouth',1)" onMouseOut="ShowPicture('mouth',0)" href=""><div  class="nav_button">code</div></a></td>
+				    		
+							<td ><a onMouseOver="ShowPicture('batman',1)" onMouseOut="ShowPicture('batman',0)" href=""><div class="nav_button">amurica</div></a></td>
+
+				    		<td ><a onMouseOver="ShowPicture('juggler',1)" onMouseOut="ShowPicture('juggler',0)" href=""><div class="nav_button">juggling</div></a></td> 
+						
+							<td ><a onMouseOver="ShowPicture('happyBull',1)" onMouseOut="ShowPicture('happyBull',0)" href=""><div class="nav_button">foto</div></a></td> 
+
+				 			<td ><a onMouseOver="ShowPicture('iAmJoe',1)" onMouseOut="ShowPicture('iAmJoe',0)" href="http://cs.mills.edu/~jmckenney/about/about.php"><div class="nav_button">naked</div></a></td> 
+								
+						</tr> 
+
+					</table> 
+				</div>
+				<!--End of Navigation Bar -->
+				
+	  			<div class="pic"
+				<a href="http://cs.mills.edu/~jmckenney/"><img src="face.png" ></a> 
+				</div>
+			
+				<div id="mouth">
+				<a href="http://cs.mills.edu/~jmckenney/"><img src="mouth.png" ></a> 
+				</div>
+				
+				<div id="batman">
+				<a href="http://cs.mills.edu/~jmckenney/"><img src="batman.png" ></a> 
+				</div>
+				
+				<div id="juggler">
+				<a href="http://cs.mills.edu/~jmckenney/"><img src="juggler1.png" ></a> 
+				</div>
+				
+				<div id="happyBull">
+				<a href="http://cs.mills.edu/~jmckenney/"><img src="happyBull.png" ></a> 
+				</div>
+				
+				<div id="iAmJoe">
+				<a href="http://cs.mills.edu/~jmckenney/"><img src="joe.png" ></a> 
+				</div>
+		</div>
+		<!--End of BodyBox -->
+		
+	</body>
